aac63f4a4168092151e28542e0c477b4572cc67f
theyadev <59524858+theyadev@users.noreply.github.com>
2021-02-06 17:04:52 +0100

    UPDATE: Ajout de la categorie Anime
    Pour le moment ligne 84 j'ai mit ma liste mais faudras changer ca

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ const store = new Vuex.Store({
      //socket : io("localhost:3001"),
     socket: io("https://pixelmage-back.herokuapp.com"),
     connected: false,
-    categories: ["TOUTES", "Disney", "Célébrités"],
+    categories: ["TOUTES", "Disney", "Célébrités", "Anime"],
     images: [
       { image: "roi_lion.jpg", reponse: "Le Roi Lion", categorie: "Disney" },
       { image: "hercule.jpg", reponse: "Hercule", categorie: "Disney" },
@@ -58,6 +58,75 @@ const store = new Vuex.Store({
 })
 
 
+var query = `
+query ($userName: String) {
+  anime: MediaListCollection(userName: $userName, type: ANIME) {
+    lists {
+      entries {
+        media {
+          coverImage {
+            extraLarge
+            color
+          }
+          title {
+            romaji
+          }
+        }
+      }
+    }
+  }
+}
+
+`;
+
+// Define our query variables and values that will be used in the query request
+var variables = {
+  userName: "Theya",
+};
+
+// Define the config we'll need for our Api request
+var url = "https://graphql.anilist.co",
+  options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query: query,
+      variables: variables,
+    }),
+  };
+
+// Make the HTTP Api request
+fetch(url, options)
+  .then(handleResponse)
+  .then(handleData)
+  .catch(handleError);
+
+function handleResponse(response) {
+  return response.json().then(function(json) {
+    return response.ok ? json : Promise.reject(json);
+  });
+}
+
+function handleData(data) {
+  const completedEntries = data.data.anime.lists[0].entries;
+  console.log(completedEntries[0]);
+  completedEntries.forEach((e) => {
+    console.log(e.media.title.romaji);
+    store.state.images.push({
+      image: e.media.coverImage.extraLarge,
+      reponse: e.media.title.romaji,
+      categorie: "Anime",
+    });
+  });
+}
+
+function handleError(error) {
+  alert("Error, check console");
+  console.error(error);
+}
 
 
 new Vue({
